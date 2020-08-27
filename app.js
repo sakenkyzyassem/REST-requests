ad08a59a00760d291f24e25c66dc5a72c830bb71
sakenkyzy.assem <sakenkyzy.assem@gmail.com>
2020-08-27 15:12:10 +0600

    users get, post, add methods - need to work on put

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError   = require('http-errors');
+var express        = require('express');
+var path           = require('path');
+var cookieParser   = require('cookie-parser');
+var logger         = require('morgan');
+var methodOverride = require("method-override");
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -12,12 +13,14 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+app.set("json spaces", 2);
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(methodOverride("_method"));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,77 @@
 var express = require('express');
 var router = express.Router();
+const axios = require("axios");
+
+var users = []
+
+function createError (res, status, message) {
+  var err = new Error(message);
+  err.status = status;
+  res.render('error', {message, err})
+}
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('GET request to the homepage')
+router.get('/', (req, res, next) => {
+  axios.get("https://jsonplaceholder.typicode.com/users")
+    .then( (response) => {
+      var serverUsers = response.data;
+      res.render('users', {serverUsers, users});
+    }).catch((error) => {
+      res.json("Error occured!")
+    }); 
+});
+
+router.get('/add', (req, res, next) => {
+  res.render('form');
+})
+
+router.post('/add', (req, res, next) => {
+  if( !req.body ){
+    res.send('Incorrect form. Try again');
+  }
+  users.push(req.body);
+  res.render('index', { title: 'Express' });
+})
+
+router.get('/edit/:id', (req, res, next) => {
+  var id = req.params.id;
+
+  if( !id ){
+    var userMessage="No index found. Try again"
+    res.render('users', {userMessage, users})
+  }
+  var user = users[id-11];
+
+  res.render('edit', {id, user});
+});
+
+router.put('edit/:id', (req, res, next) => {
+  if( !req.body ){
+    var userMessage="Changes were not saved. Try again"
+    res.render('users', {userMessage, users})
+  }
+  Object.assign(users[req.params.id-11], req.body);
+
+  res.render('index', {title: Express});
+});
+
+router.get('/:id', (req, res, next) => {
+  if(!req.params.id) {
+    createError(res, 404, 'No user username');
+  }
+
+  if( req.params.id > 10 ){
+    res.json(users[req.params.id-11]);
+    return;
+  }
+
+  axios.get(`https://jsonplaceholder.typicode.com/users/${req.params.id}`)
+    .then( (response) => {
+      res.json(response.data);
+    })
+    .catch( (err) => {
+      createError(res, err.status, err.message);
+    })
 });
 
 module.exports = router;
